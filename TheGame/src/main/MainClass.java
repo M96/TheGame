1af0e7d4ba469f883825ef857ad40142df47f8fd
Markus <farbklecks96@aol.com>
2015-08-04 18:17:34 +0200

    Hello

--- TheGame/src/main/MainClass.java
+++ TheGame/src/main/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 	public MainClass() {
 		// TODOAuto-generated constructor stub
 		System.out.println("Hallo Markus"+"Was geht???");
+		System.out.println("Hallo Markus"+"Was geht???");
 	}
 
 	public static void main(String[] args) {
