1297542ce7118920fa7e3dc1c905ab492257d7e2
Markus <farbklecks96@aol.com>
2015-08-04 18:27:01 +0200

    s

--- TheGame/src/main/MainClass.java
+++ TheGame/src/main/MainClass.java
@@ -7,7 +7,7 @@ public class MainClass {
 		System.out.println("Hallo Atti"+"Was geht???");
 		System.out.println("Hallo Markus"+"Was geht???");
 		//Schwul
-		//okaydokay
+		//okaydokayy
 	}
 
 	public static void main(String[] args) {
