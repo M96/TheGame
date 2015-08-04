2d085b28f11bc623e98e7b548cc36a83a7584e91
Markus <farbklecks96@aol.com>
2015-08-04 19:43:04 +0200

    Signed-off-by: Markus <farbklecks96@aol.com>

--- TheGame/src/main/Game.java
+++ TheGame/src/main/Game.java
@@ -1,11 +1,15 @@
 package main;
 
-public class Game implements Runnable {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Game implements Runnable,KeyListener {
 
 	MainClass graphic;
 	
 	Game(MainClass parent) {
 		graphic = parent;
+		graphic.canvas.addKeyListener(this);
 	}
 
 	@Override
@@ -16,5 +20,24 @@ public class Game implements Runnable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode()==e.VK_ESCAPE) {
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
