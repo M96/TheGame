e2973f869c9bbb279d3652eb1ab381542bf5d412
Markus <farbklecks96@aol.com>
2015-08-05 17:07:12 +0200

    Signed-off-by: Markus <farbklecks96@aol.com>

--- TheGame/src/main/Ui.java
+++ TheGame/src/main/Ui.java
@@ -1,14 +1,34 @@
 package main;
 
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
 
 public class Ui{
 	
 	BufferedImage image;
 	
-	Ui()
+	private Button theButton;
+	
+	
+	Ui(JFrame frame)
 	{
-		image = BiLoader.loadPics("test.png", 1)[0];
+		//image = BiLoader.loadPics("Lighthouse.jpg", 1)[0];
+		
+		theButton = new Button("The Button");
+		theButton.setBounds(50, 50, 60, 20);
+		theButton.setVisible(true);
+		theButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(e.getActionCommand());
+			}
+		});
+		frame.add(theButton);
 	}
 	
 }
